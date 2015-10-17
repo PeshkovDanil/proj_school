923fe98d658f8082fb9d0edfcfb54bacaa02d981
peshkov <danilpeshkov014@gmail.com>
2015-10-17 18:59:54 +0700

    Сделано, можно пофиксить

--- Task.py
+++ Task.py
@@ -1,9 +1,11 @@
 import json
 from pprint import pprint
 
-def search(list_of,**kwargs):
+def search( list_of, **kwargs):
     list1=[]
     new_list=[]
+    if kwargs.get('class_room'):
+        kwargs['class']=kwargs.pop('class_room')
     list1=kwargs.keys()
     for el in list1:
         for l in list_of:
@@ -22,8 +24,13 @@ list2 = []
 list1 = []
 p = 1
 schools = set()
-teach = json.load(open('Teachers_id.json'))
-stud = json.load(open('Students_id.json'))
+# teach = json.load(open('Teachers_id.json'))
+# stud = json.load(open('Students_id.json'))
+
+with open('Students_id.json', 'r') as f:
+   stud = json.load(f)
+with open('Teachers_id.json','r') as f:
+    teach=json.load(f)
 
 cls = '6 А'
 
@@ -49,7 +56,7 @@ for el in list1:
     for l in list1[p:]:
         if el == l:
             print('однофомильцы:', el)
-    p = p + 1
+    p=p + 1
 
 # 2.1
 print('2.1')
@@ -81,34 +88,42 @@ for el in teach:
 
 # 3.1
 # print('3.1')
-# new_stud={'name':'Дмитрий',
-#           'middle_name':'Васильевич',
-#           'surname':'Егоров',
-#           'school':'23 Школа',
-#           'class':'7 В',
-#           'birth_day':'03.12.1993'}
-# stud.append(new_stud)
-# # print(stud)
-# all_st_new=json.dumps(stud, ensure_ascii=False)
-# # stud1=open('Students_id.json','w')
-# stud1.write(all_st_new)
-# stud1.close()
+for el in stud:
+    id1=el['id']
+id1=id1+1
+new_stud={'name':'Дмитрий',
+          'middle_name':'Васильевич',
+          'surname':'Егоров',
+          'school':'23 Школа',
+          'class':'7 В',
+          'birth_day':'03.12.1993',
+           'id':id1}
+stud.append(new_stud)
+# print(stud)
+all_st_new=json.dumps(stud, ensure_ascii=False)
+with open('Students_id.json','w') as stud1:
+    stud1.write(all_st_new)
+    
 
 
 # 3.2
 print('3.2')
-new_teach = {'name': 'Дмитрий',
+for el in teach:
+    id=el['id']
+id=id+1
+new_teach = {'name': 'Евгений',
              'middle_name': 'Васильевич',
-             'surname': 'Егоров',
-             'school': '23 Школа',
-             'class': '7 В',
-             'birth_day': '03.12.1993'}
+             'surname': 'Романов',
+             'school': '43 Школа',
+             'class': '6 В',
+             'birth_day': '03.12.1993',
+             'id':id}
 teach.append(new_teach)
 # print(teach)
 all_teach_new = json.dumps(teach, ensure_ascii=False)
-# teach1=open('Teachers_id.json','w')
-# teach1.write(all_teach_new)
-# teach1.close()
+with open('Teachers_id.json','w') as teach1:
+    teach1.write(all_teach_new)
+    
 
 # 3.3
 print('3.3')
@@ -118,9 +133,9 @@ teach_index = get_index(teach, surname)
 teach[teach_index]['class'].append(new_class)
 # print(teach)
 new_class_teach = json.dumps(teach, ensure_ascii=False)
-# teach1=open('Teachers_id.json','w')
-# teach1.write(new_class_teach)
-# teach1.close()
+with open('Teachers_id.json','w') as teach1:
+    teach1.write(new_class_teach)
+    
 
 # 3.4
 print('3.4')
@@ -131,10 +146,10 @@ for el in stud:
     if del_stud_list[0] == el['surname'] and del_stud_list[1] == el['name']:
         # print(el)
         stud.remove(el)
-        # all_st_new=json.dumps(stud, ensure_ascii=False)
-        # stud1.write(all_st_new)
-        # print(stud)
-        # stud1.close()
+        all_st_new=json.dumps(stud, ensure_ascii=False)
+        with open('Students_id.json','w') as stud1:
+            stud1.write(all_st_new)
+            
 
 # 3.5
 print('3.5')
@@ -143,10 +158,10 @@ for el in stud[:]:
     if el['class'] == del_class:
         print(el)
         stud.remove(el)
-# stud1=open('Students_id.json','w')
-# new_class=json.dumps(stud, ensure_scii=False)
-# stud1.write(new_class)
-# stud1.close()
+new_class=json.dumps(stud, ensure_ascii=False)
+with open('Students_id.json','w') as stud1:
+    stud1.write(new_class)
+    
 
 # 3.6
 print('3.6')
@@ -158,10 +173,10 @@ for el in teach[:]:
         # print(el)
         teach.remove(el)
         # print(teach)
-        # teach1=open('Teachers_id.json','w')
-        # new_teach=json.dumps(teach, ensure_ascii=False)
-        # teach1.write(new_teach)
-        # teach1.close()
+        new_teach=json.dumps(teach, ensure_ascii=False)
+        with open('Teachers_id.json','w') as teach1:
+            teach1.write(new_teach)
+            
 
 # 3.7
 print('3.7')
@@ -169,12 +184,10 @@ school = '67 школа'
 for el in teach[:]:
     if school == el['school']:
         teach.remove(el)
-        # teach1=open('Teachers_id.json','w')
-        # new_teach=json.dumps(teach, ensure_ascii=False)
-        # teach1.write(new_teach)
-        # teach1.close()
-        print(teach)
-
+        new_teach=json.dumps(teach, ensure_ascii=False)
+        with open('Teachers_id.json','w') as teach1:
+            teach1.write(new_teach)
+            
 # 3.8
 print('3.8')
 teach_class = '6 Б'
@@ -190,10 +203,10 @@ for el in teach_class_list[:]:
 for el in teach:
     if el['surname'] == teach_name[0] and el['name'] == teach_name[1]:
         el['class'] = teach_class_list
-# teach1=open('Teachers_id.json','w')
-# new_class_list=json.dumps(teach, ensure_ascii=False)
-# teach1.write(new_class_list)
-# teach1.close()
+new_class_list=json.dumps(teach, ensure_ascii=False)
+with open('Teachers_id.json','w') as teach1:
+    teach1.write(new_class_list)
+    
 
 # 4.2
 print('4.2')
@@ -211,3 +224,12 @@ for el in id_list:
 print('4.3')
 a=search(stud,name='Кузин',class_room='8 Д')
 print(a)
+
+# 4.4
+print('4.4')
+teachers_class='7 В'
+for el in teach:
+    for l in el['class']:
+        if teachers_class==l:
+            print(el['id'])
+
